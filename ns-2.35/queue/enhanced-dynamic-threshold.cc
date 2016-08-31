818f6d546b77cb325814d1739f1ad3a9ab29d230
Danfeng Shan <dfshan@csnet1.cs.tsinghua.edu.cn>
2016-08-31 16:18:23 +0800

    change filename

---
+++ ns-2.35/queue/enhanced-dynamic-threshold.cc
@@ -0,0 +1,166 @@
+#ifndef lint
+static const char rcsid[] =
+    "@(#) $Header: /cvsroot/nsnam/ns-2/queue/enhanced-dynamic-threshold.cc,v 1.0 2014/04/10 20:00:00 haldar Exp $ (LBL)";
+#endif
+
+#include "enhanced-dynamic-threshold.h"
+
+// initialize the static members
+EnhancedDynamicThreshold* EnhancedDynamicThreshold::all_queue[MAX_LINK_NUM] = {NULL};
+int EnhancedDynamicThreshold::queue_num = 0;
+int EnhancedDynamicThreshold::edt_num = 0;
+
+static class EnhancedDynamicThresholdClass : public TclClass {
+ public:
+	EnhancedDynamicThresholdClass() : TclClass("Queue/EnhancedDynamicThreshold") {}
+	TclObject* create(int, const char*const*) {
+		return (new EnhancedDynamicThreshold);
+	}
+} class_enhanced_dynamic_threshold;
+
+
+int EnhancedDynamicThreshold::adj_counters()
+{
+	double now_time = NOW_TIME;
+	double elasped_time1 = now_time - trigger_time1;
+	double elasped_time2 = now_time - trigger_time2;
+
+	// adjust timer1: the traffic is not bursty
+	if(trigger_time2 < 0 && elasped_time1 >= INIT_TIMER1) {
+		counter1 = 0;
+		counter2 = 0;
+		trigger_time1 = now_time;
+	}
+
+	// adjust timer2: timeout, change to controlled state
+	if(trigger_time2 >= 0 && elasped_time2 >= INIT_TIMER2) {
+		to_controlled();
+	}
+
+	return (trigger_time2 >= 0 ? 0 : 1);
+}
+
+/*
+ * drop-tail
+ */
+void EnhancedDynamicThreshold::printque(char pre)
+{
+	if (DISQUE < 0) {
+		return ;
+	} else if (DISQUE == 0 && pre != 'd') {
+		return ;
+	}
+	int quelen = get_occupied_mem(queue_id);
+	// <flag> <time in s> <queue id> <queue length in pkts> <occupied buffer size in pkts>
+	printf("%c %f %d %d %d %.3f %d\n", pre, NOW_TIME/1000.0, queue_id, all_queue[queue_id] -> q_->length(), quelen, 1.0*quelen/BUFFER_SIZE, edt_num);
+}
+
+void EnhancedDynamicThreshold::enque(Packet* p) {
+	double threshold = adj_counters()?get_threshold(queue_id):(1.0 * BUFFER_SIZE/edt_num);
+	/*0: no drop; 1: drop and qlen > threshold  2: drop and buffer overflow*/
+	int is_drop = 0;
+	if (get_occupied_mem(queue_id) > BUFFER_SIZE) {
+		drop(p);
+		printque('d');
+		is_drop = 2;
+	} else if (q_->length() > threshold) {
+		drop(p);
+		printque('d');
+		is_drop = 1;
+	} else {
+		q_->enque(p);
+		printque('+');
+	}
+	switch (is_drop) {
+		case 0:
+			counter1 = 0;
+			/*Change to the uncontrolled state*/
+			if (trigger_time2 < 0 && ++counter2 >= COUNTER2) {
+				to_uncontrolled();
+			}
+			//if (queue_id == 3)
+			//	printf("time:%.8f enqueue counter1:%d counter2:%d trigger_time1:%.2f trigger_time2: %.2f\n", NOW_TIME, counter1, counter2, trigger_time1, trigger_time2);
+			break;
+		case 1:
+			counter1 = 0;
+			counter2 = 0;
+			break;
+		case 2:
+			/*Buffer overflows, change all ports to controlled state*/
+			to_controlled_all();
+			//if (queue_id == 3)
+			//	printf("time:%.8f dropped counter1:%d counter2:%d trigger_time1:%.2f trigger_time2: %.2f\n", NOW_TIME, counter1, counter2, trigger_time1, trigger_time2);
+			break;
+	}
+}
+
+Packet* EnhancedDynamicThreshold::deque()
+{
+	adj_counters();
+
+	Packet* result = q_->deque();
+	int quelen = get_occupied_mem(queue_id);
+	printque('-');
+	if( result != 0 ) {
+		counter1 ++;
+		if (trigger_time2 >= 0) {
+			/*Port becomes underloaded. Change to the controlled state*/
+			if (counter1 >= COUNTER1) {
+				to_controlled();
+			}
+		} else {
+			counter2 --;
+			counter2 = (counter2 < 0 ? 0 : counter2);
+		}
+	}
+	//if (queue_id == 3)
+	//	printf("time:%.8f dequeue counter1:%d counter2:%d trigger_time1:%.2f trigger_time2: %.2f\n", NOW_TIME, counter1, counter2, trigger_time1, trigger_time2);
+	return result;
+}
+
+int EnhancedDynamicThreshold::get_occupied_mem(int id)
+{
+	// for this switch, the ports' id are start, start+1, ..., start + SWITCH_PORTS
+	int start = (id / SWITCH_PORTS) * SWITCH_PORTS;
+	int occupy = 0;
+	for(int i = start; i < start + SWITCH_PORTS && i < queue_num; i++ ) {
+		occupy += all_queue[i] -> q_->length();
+	}
+	return occupy;
+}
+
+double EnhancedDynamicThreshold::get_threshold(int id)
+{
+	double threshold = get_occupied_mem(id);
+	threshold = ALPHA * (BUFFER_SIZE - threshold);
+	return threshold > 0 ? threshold : 0;
+}
+
+void EnhancedDynamicThreshold::to_controlled() {
+	if (trigger_time2 >= 0 ) {
+		counter1 = 0;
+		counter2 = 0;
+		trigger_time2 = -1;
+		trigger_time1 = NOW_TIME;
+		edt_num --;
+	}
+}
+
+void EnhancedDynamicThreshold::to_uncontrolled() {
+	if (trigger_time2 < 0 ) {
+		long now_time = NOW_TIME;
+		trigger_time2 = now_time;
+		trigger_time1 = now_time;
+		edt_num ++;
+		counter2 = 0;
+		counter1 = 0;
+	}
+}
+
+void EnhancedDynamicThreshold::to_controlled_all() {
+	// for this switch, the ports' id are start, start+1, ..., start + SWITCH_PORTS
+	int start = (queue_id / SWITCH_PORTS) * SWITCH_PORTS;
+	for(int i = start; i < start + SWITCH_PORTS && i < queue_num; i++ ) {
+		all_queue[i] -> to_controlled();
+	}
+}
---
+++ ns-2.35/queue/enhanced-dynamic-threshold.h
@@ -0,0 +1,85 @@
+/* -*-	Mode:C++; c-basic-offset:8; tab-width:8; indent-tabs-mode:t -*- */
+/*
+ * Copyright (c) 1994 Regents of the University of California.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the Computer Systems
+ *	Engineering Group at Lawrence Berkeley Laboratory.
+ * 4. Neither the name of the University nor of the Laboratory may be used
+ *    to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * @(#) $Header: /cvsroot/nsnam/ns-2/queue/drop-tail.h,v 1.19 2004/10/28 23:35:37 haldar Exp $ (LBL)
+ */
+
+#ifndef ns_enhanced_dynamic_threshold_h
+#define ns_enhanced_dynamic_threshold_h
+
+#include <string.h>
+#include <sys/time.h>
+#include "queue.h"
+#include "config.h"
+#include "scheduler.h"
+#include "myhead.h"
+
+class EnhancedDynamicThreshold : public Queue {
+  public:
+	EnhancedDynamicThreshold() {
+		q_ = new PacketQueue;
+		pq_ = q_;
+		counter1 = 0;
+		counter2 = 0;
+		trigger_time1 = NOW_TIME;
+		trigger_time2 = -1;
+		queue_id = queue_num++;
+		all_queue[queue_id] = this;
+	}
+	~EnhancedDynamicThreshold() {
+		delete q_;
+	}
+
+  protected:
+	void enque(Packet*);
+	Packet* deque();
+	static int get_occupied_mem(int id); // get threshold of queue id
+	static double get_threshold(int id); // get threshold of queue id
+	void to_controlled(); // change to the controlled state
+	void to_uncontrolled(); // change to the uncontrolled state
+	void to_controlled_all(); // change all of the port to the controlled state
+	void printque(char pre);
+
+	PacketQueue* q_;
+	int adj_counters(); // adjast counters before enqueue and dequeue, return 0 if timer2 is still running
+	static EnhancedDynamicThreshold* all_queue[MAX_LINK_NUM]; /* store all of the queues */
+	static int queue_num; /* the number of queues */
+	static int edt_num; /* the number of ports in edt state */
+	int queue_id;
+	int counter1; // value of counter1
+	int counter2; // value of counter2
+	double trigger_time1; // trigger time of timer1 in milliseconds
+	double trigger_time2; // trigger time of timer2 in milliseconds
+};
+
+#endif
